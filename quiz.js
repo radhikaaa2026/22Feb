070e0980153b652676a0642cad91aef1182e7a95
radhikaaa2026 <160358499+radhikaaa2026@users.noreply.github.com>
2024-02-18 01:57:10 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,222 @@
+let questions = [ 
+    {
+        num: 1, 
+        question: "Which nickname I have loved the most uptil now given by you??🤔💕",
+        answer:"B. Tiddi",
+        options:[
+            "A. Bandarr",
+            "B. Tiddi",
+            "C. Gussamon",
+            "D. Radio"
+        ]
+    },
+    {
+        num: 2, 
+        question: "On which song did I first taught u dance?💃🕺",
+        answer:"C. Heeriye",
+        options:[
+            "A. What Jhumka!",
+            "B. Tere Bina",
+            "C. Heeriye",
+            "D. Aa ke Dekhle"
+        ]
+    },
+    {
+        num: 3, 
+        question: "When did we have our first fight?🤔🤦‍♀️",
+        answer:"D. July",
+        options:[
+            "A. October",
+            "B. December",
+            "C. April",
+            "D. July"
+        ]
+    },
+    {
+        num: 4, 
+        question: "What was our plan on our first day of realtionship which you missed?",
+        answer:"B. Bunk",
+        options:[
+            "A. Date",
+            "B. Bunk",
+            "C. Class",
+            "D. McD"
+        ]
+    },
+    {
+        num: 5, 
+        question: "Where was the third time you gave a peck on my right cheek?🤭🤭",
+        answer:"A. Lt",
+        options:[
+            "A. Lt",
+            "B. Class backseat",
+            "C. Auto to hapur",
+            "D. Flat"
+        ]
+    },
+    {
+        num: 6, 
+        question: "What about the first on left one??😂🤨🩵",
+        answer:"B. Holi Ghar",
+        options:[
+            "A. Auto",
+            "B. Holi Ghar",
+            "C. Class Backseat",
+            "D. Library"
+        ]
+    },
+    {
+        num: 7, 
+        question: "What's according to you, is my idea of the perfect date night?(Jo kabhi baad me chalenge abhi nahi😂😂😂)",
+        answer:"B. Concert and outings",
+        options:[
+            "A. Candle-light dinner",
+            "B. Concert and outings",
+            "C. Movie night",
+            "D. Gaming zone"
+        ]
+    },
+    {
+        num: 8, 
+        question: "Color of the first flower that I gave you?",
+        answer:"D. White",
+        options:[
+            "A. Yellow",
+            "B. Purple",
+            "C. Red",
+            "D. White"
+        ]
+    },
+    {
+        num: 9, 
+        question: "What is the first thing I do in the morning, when I'm with you?",
+        answer:"B. Snuggle",
+        options:[
+            "A. Kiss your cheek..",
+            "B. Snuggle",
+            "C. Check for my phone",
+            "D. Wake you up"
+        ]
+    },
+    {
+        num: 10, 
+        question: "What's my weirdest habit which you said you love the most??😂😂😂",
+        answer:"C. Doing random dance moves",
+        options:[
+            "A. Naming inanimate objects",
+            "B. Collecting random items",
+            "C. Doing random dance moves",
+            "D. Speaking in different accents"
+        ]
+    },
+    {
+        num: 11, 
+        question: "Biggest hint giver to our relationship was..??",
+        answer:"C. Sristhi",
+        options:[
+            "A. Trapti",
+            "B. Laavanya",
+            "C. Sristhi",
+            "D. Jesmine"
+        ]
+    },
+    {
+        num: 12, 
+        question: "What weakness of mine gave me the best moment with you on the Manali Trip??😍🩵(gonna kill u for this one..)",
+        answer:"D. Short height",
+        options:[
+            "A. Impatience",
+            "B. Overthinking",
+            "C. Lack of sleep",
+            "D. Short height"
+        ]
+    },
+    {
+        num: 13, 
+        question: "Will I go out on an outing..Or will I choose my sleep over the outing?(until and unless me ekdum badiya..and not beemar)",
+        answer:"A. Outing damn sure",
+        options:[
+            "A. Outing damn sure",
+            "B. Neend bhaii neend",
+            "C. Can't say moody ekdum",
+            "D. Depends kab or kaha jaana hai"
+        ]
+    },
+    {
+        num: 14, 
+        question: "Only twinning that we decided in college to do?",
+        answer:"D. Gray",
+        options:[
+            "A. Black",
+            "B. Blue",
+            "C. Red",
+            "D. Gray"
+        ]
+    },
+    {
+        num: 15, 
+        question: "What was I wearing when you last met me now..??",
+        answer:"C. Mahroon",
+        options:[
+            "A. Green",
+            "B. Black",
+            "C. Mahroon",
+            "D. White"
+        ]
+    },
+    {
+        num: 16, 
+        question: "What's your contact photo on my mobile-phone?",
+        answer:"D. Convocation",
+        options:[
+            "A. Christmas",
+            "B. Farewell",
+            "C. Trip",
+            "D. Convocation"
+        ]
+    },
+    {
+        num: 17, 
+        question: "What color combo are 'you' and 'me' wearing in my whatsapp background image of your chat?",
+        answer:"A. Blue and black",
+        options:[
+            "A. Blue and black",
+            "B. Black and blue",
+            "C. Black and mehroon",
+            "D. Pink and white"
+        ]
+    },
+    {
+        num: 18, 
+        question: "The moment when I first time hugged you..??",
+        answer:"A. Holi",
+        options:[
+            "A. Holi",
+            "B. Date",
+            "C. After returning from home",
+            "D. Not these....June"
+        ]
+    },
+    {
+        num: 19, 
+        question: "Out of all true options, which is not my fetish?",
+        answer:"B. Getting my friends into relationships",
+        options:[
+            "A. Randomly knowing bollywood movie dialogues",
+            "B. Getting my friends into relationships",
+            "C. Knowing every gossip",
+            "D. Dancing to cheesy bollywood songs with correct steps"
+        ]
+    },
+    {
+        num: 20, 
+        question: "(Just Bonus)At What time you propsed me?",
+        answer:"A. 12:23",
+        options:[
+            "A. 12:23",
+            "B. 01:04",
+            "C. 12:37",
+            "D. 01:15"
+        ]
+    }
+];
---
+++ script.js
@@ -0,0 +1,157 @@
+const startButton = document.querySelector('.start-btn');
+const popup = document.querySelector('.popup');
+const exitButton = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueButton= document.querySelector('.continue-btn');
+const quiz = document.querySelector('.quiz-section');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
+const  nextBtn = document.querySelector('.next-btn');
+const quizOptions=document.querySelector('.quiz-options');
+const result = document.querySelector('.result');
+const tryBtn = document.querySelector('.tryagain-btn');
+const homeBtn = document.querySelector('.home-btn');
+
+startButton.onclick = () => {
+    popup.classList.add('active');
+    main.classList.add('active');
+}
+exitButton.onclick =() => {
+    popup.classList.remove('active');
+    main.classList.remove('active');
+}
+continueButton.onclick= () => {
+    quiz.classList.add('active');
+    popup.classList.remove('active');
+    main.classList.remove('active');
+    quizBox.classList.add('active');
+
+    showQues(0);
+    quesCounter(1);
+    headerScore();
+}
+tryBtn.onclick =() => {
+    quizBox.classList.add('active');
+    result.classList.remove('active');
+    nextBtn.classList.remove('active');
+    
+    quesCnt = 0;
+    score=0;
+    showQues(quesCnt);
+    quesCounter(quesCnt+1);
+
+    headerScore();
+}
+homeBtn.onclick = () => {
+    quizSection.classList.remove('active');
+    result.classList.remove('active');
+    nextBtn.classList.remove('active');
+    
+    quesCnt = 0;
+    score=0;
+    showQues(quesCnt);
+    quesCounter(quesCnt+1);
+
+}
+
+
+let  quesCnt = 0;
+let score=0;
+
+nextBtn.onclick= () => {
+    if(quesCnt < questions.length - 1){
+    quesCnt++;
+    
+    showQues(quesCnt);
+    quesCounter(quesCnt+1);
+
+    nextBtn.classList.remove('active');
+    }
+    else{
+        showResult();
+    }
+}
+
+//ques from array with options
+
+function showQues(index){
+    const quesText = document.querySelector('.ques-text');
+    quesText.textContent = `${questions[index].num}. ${questions[index].question}`;
+
+    let optionTag = `<div class="options"><span>${questions[index].options[0]}</span></div>
+    <div class="options"><span>${questions[index].options[1]}</span></div>
+    <div class="options"><span>${questions[index].options[2]}</span></div>
+    <div class="options"><span>${questions[index].options[3]}</span></div>`;
+
+    quizOptions.innerHTML = optionTag;
+
+    const option = document.querySelectorAll('.options');
+    for(let i=0;i<option.length ; i++)
+    {
+        option[i].setAttribute('onclick','optionSelected(this)');
+    }
+}
+
+function optionSelected(ans){
+    let userAns = ans.textContent;
+    let correctAns = questions[quesCnt].answer;
+    let allOptions = quizOptions.children.length;
+    
+    if(userAns == correctAns){
+        ans.classList.add('correct');
+        score++;
+        headerScore();
+    }
+    else{
+        ans.classList.add('incorrect');
+        for(let i=0;i<allOptions;i++){
+            if(quizOptions.children[i].textContent == correctAns)
+            {
+                quizOptions.children[i].setAttribute('class','options correct');
+            }
+        }
+    }
+
+    //if user selects anything disable rest 
+    for(let i=0;i<allOptions;i++)
+    {
+        quizOptions.children[i].classList.add('disabled');
+    }
+
+    nextBtn.classList.add('active');
+    
+}
+
+function quesCounter(index){
+    const totalQues = document.querySelector('.ques-total');
+    totalQues.textContent = `${index} of ${questions.length} Questions`;
+}
+
+function headerScore() {
+     const headerScoreText = document.querySelector('.header-score');
+     headerScoreText.textContent = `Score: ${score} / ${questions.length}`;
+}
+
+function showResult(){
+    quizBox.classList.remove('active');
+    result.classList.add('active');
+
+    const scoreText = document.querySelector('.score-text');
+    scoreText.textContent = `You scored ${score} out of ${questions.length}`;
+
+    const progress = document.querySelector('.progress');
+    const progressVal = document.querySelector('.progress-value');
+    let progStartVal = -1;
+    let progEndVal = (score/questions.length)*100;
+    let speed=20;
+    let prog = setInterval( () => {
+        progStartVal++;
+        
+        progressVal.textContent = `${progStartVal}%`;
+        progress.style.background = `conic-gradient(#c45c00 ${progStartVal *3.6}deg,rgba(255,255,255,.1) 0deg)`;
+
+        if(progStartVal == progEndVal){
+            clearInterval(prog);
+        }
+    },speed)
+}
